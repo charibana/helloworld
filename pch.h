0499e54b9799adc5eafb58b6dd256578f6ba0cbe
charibana <101860703+charibana@users.noreply.github.com>
2022-05-02 14:25:07 +0800

    Create pch.h

---
+++ pch.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef PCH_H
+#define PCH_H
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#endif //PCH_H
