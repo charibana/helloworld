14033297c4d01f6f6a1b06b7160c34d4d609ceb4
charibana <101860703+charibana@users.noreply.github.com>
2022-05-02 18:41:59 +0800

    Update work4_main.c

--- work4_main.c
+++ work4_main.c
@@ -1,5 +1,17 @@
+#pragma once
 
-#include "pch.h"
+//#ifndef PCH_H
+//#define PCH_H
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+//#endif //PCH_H
 using namespace std;
 
 static std::string roundAny(float r, int precision) {
